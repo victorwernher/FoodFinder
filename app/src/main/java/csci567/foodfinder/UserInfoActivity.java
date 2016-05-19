c5383b8e0ae0d938a263926d54eb7c9c0f3423e9
Bradley Esselman <besselman.mail.csuchico.edu>
2016-05-18 22:42:33 -0700

    Added remove button on User info

--- app/src/main/java/csci567/foodfinder/UserInfoActivity.java
+++ app/src/main/java/csci567/foodfinder/UserInfoActivity.java
@@ -2,10 +2,16 @@ package csci567.foodfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
 
     private int user_id;
+    private ArrayList<Restaurant> m_likes = new ArrayList<>();
+    private ArrayList<Restaurant> m_dislikes = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,18 @@ public class UserInfoActivity extends AppCompatActivity {
         {
             user_id = extras.getInt("user_id");
         }
+
+        /*
+        TODO Get data from database set equal to m_likes & m_dislikes
+         */
+
+        ListView likes = (ListView) findViewById(R.id.like_list);
+        assert likes != null;
+        likes.setAdapter(new RestaurantDataAdapter(this, m_likes));
+        ListView dislikes = (ListView) findViewById(R.id.dislike_list);
+        assert dislikes != null;
+        likes.setAdapter(new RestaurantDataAdapter(this, m_dislikes));
+
+
     }
 }
